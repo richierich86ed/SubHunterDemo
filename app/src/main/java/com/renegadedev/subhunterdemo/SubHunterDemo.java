353bfb19e48419934fa4574fd7fbb188cd6e600f
richierich86ed <rjfarley86@gmail.com>
2024-10-02 09:15:37 -0400

    added initialized variables for different screen resolutions, added LogCat notes for tracking.

--- app/src/main/java/com/renegadedev/subhunterdemo/SubHunterDemo.java
+++ app/src/main/java/com/renegadedev/subhunterdemo/SubHunterDemo.java
@@ -50,14 +50,31 @@ public class SubHunterDemo extends Activity {
         draw();
         }
 
+    /*
+        This code will execute when a new
+        game needs to be started. It will
+        happen when the app is first started
+        and after the player wins a game.
+     */
     void newGame() {
         Log.d("Debugging", "In newGame");
     }
 
+    /*
+        Here we will do all the drawing.
+        The grid lines, the HUD and
+        the touch indicator.
+     */
     void draw() {
         Log.d("Debugging", "In draw");
+        printDebuggingText();
     }
 
+    /*
+        This part of the code will
+        handle detecting that the player
+        has tapped the screen.
+     */
     @Override public boolean onTouchEvent(MotionEvent motionEvent){
         Log.d("Debugging", "In onTouchEvent");
 
@@ -67,15 +84,42 @@ public class SubHunterDemo extends Activity {
         return true;
     }
 
+    /*
+        The code here will execute when
+        the player taps the screen. It will
+        calculate the distance from teh sub'
+        and decide a hit or miss.
+     */
     void takeShot() {
 
     }
 
+    /*
+        This code says "Boom!".
+     */
     void boom() {
 
     }
 
-    void printDebugging() {
+    /*
+        This code prints the debugging text.
+     */
+    void printDebuggingText() {
+        Log.d("numberHorizontalPixels", "" + numberHorizontalPixels);
+        Log.d("numberVerticalPixels", "" + numberVerticalPixels);
 
+        Log.d("blockSize", "" + blockSize);
+        Log.d("gridWidth", "" + gridWidth);
+        Log.d("gridHeight", "" + gridHeight);
+        Log.d("horizontalTouched", "" + horizontalTouched);
+        Log.d("verticalTouched", "" + verticalTouched);
+        Log.d("subHorizontalPosition", "" + subHorizontalPosition);
+        Log.d("subVerticalPosition", "" + subVerticalPosition);
+
+        Log.d("hit", "" + hit);
+        Log.d("shotsTaken", "" + shotsTaken);
+        Log.d("debugging", "" + debugging);
+
+        Log.d("distacnceFromSub", "" + distanceFromSub);
     }
 }
